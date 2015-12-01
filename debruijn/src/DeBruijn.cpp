85c6ff2b894f4f48ae208caabeebdabd7ba150fb
Aleks Peshkov <aleks.peshkov@gmail.com>
2015-12-01 18:57:36 +0300

    Generator experiments

--- debruijn/src/DeBruijn.cpp
+++ debruijn/src/DeBruijn.cpp
@@ -30,7 +30,11 @@ bit64_t flip(bit64_t b) {
     return ~(b);
 }
 
-vector<bit64_t> deBruijn;
+#ifdef _INCLUDE
+    vector<bit64_t> deBruijn;
+#else
+#include "DeBruijn.inc"
+#endif
 
 typedef bit64_t U64;
 
@@ -161,22 +165,22 @@ bool test_d(int z, bit64_t a, bit64_t b, bit64_t c, bit64_t d) {
 bool test(int i1, int i2) {
     bit64_t a = deBruijn[i1];
     bit64_t b = deBruijn[i2];
-    for (int i = 1; i < 64; ++i) {
-        a = a << 1 | a >> 63;
-        b = b << 1 | b >> 63;
+    return test_d(14, a, b);
+}
 
-        if (!test_d(a, b, 12)) { return false; }
+bool test(int i1, int i2, int i3) {
+    bit64_t a = deBruijn[i1];
+    bit64_t b = deBruijn[i2];
+    bit64_t c = deBruijn[i3];
+    return test_d(20, a, b, c);
+}
 
-        /*bit64_t a2 = a;
-        bit64_t b2 = b;
-        for (int j = i+1; j < 64; ++j) {
-            a2 = a2 << 1 | a2 >> 63;
-            b2 = b2 << 1 | b2 >> 63;
-            if (!test_d(deBruijn[i1]^b^b2, 16)) { return false; }
-            if (!test_d(deBruijn[i2]^a^a2, 16)) { return false; }
-        }*/
-    }
-    return true;
+bool test(int i1, int i2, int i3, int i4) {
+    bit64_t a = deBruijn[i1];
+    bit64_t b = deBruijn[i2];
+    bit64_t c = deBruijn[i3];
+    bit64_t d = deBruijn[i4];
+    return test_d(28, a, b, c, d);
 }
 
 void show(int i) {
@@ -186,74 +190,67 @@ void show(int i) {
 void findCombi() {
     int size = deBruijn.size();
     for (int a = 0; a < size; ++a) {
+        cout << dec << a << endl;
         for (int b = a+1; b < size; ++b) {
-            if (!test_d(16,deBruijn[a], deBruijn[b])) { continue; }
+            if (!test(a, b)) { continue; }
 
             for (int c = b+1; c < size; ++c) {
-                if (!test_d(16,deBruijn[a], deBruijn[c]) || !test_d(16,deBruijn[b], deBruijn[c]) || !test_d(24,deBruijn[a], deBruijn[b], deBruijn[c])) { continue; }
+                if (!test(a, c) || !test(b, c) || !test(a, b, c)) { continue; }
 
                 for (int d = c+1; d < size; ++d) {
-                    if (!test_d(16,deBruijn[a], deBruijn[d]) || !test_d(16,deBruijn[b], deBruijn[d]) || !test_d(16,deBruijn[c], deBruijn[d])
-                        || !test_d(24,deBruijn[a], deBruijn[b], deBruijn[d]) || !test_d(24,deBruijn[a], deBruijn[c], deBruijn[d]) || !test_d(24,deBruijn[b], deBruijn[c], deBruijn[d])
-                        || !test_d(32,deBruijn[a], deBruijn[b], deBruijn[c], deBruijn[d])
+                    if (!test(a, d) || !test(b, d) || !test(c, d)
+                        || !test(a, b, d) || !test(a, c, d) || !test(b, c, d)
+                        || !test(a, b, c, d)
                     ) { continue; }
 
-                    show(a); show(b); show(c); show(d);
-                    cout << endl;
-
                     for (int e = d+1; e < size; ++e) {
-                        if (!test(e, d) || !test(e, c) || !test(e, b) || !test(e, a)) { continue; }
+                        if (!test(e, d) || !test(e, c) || !test(e, b) || !test(e, a)
+                            || !test(a, b, e) || !test(a, c, e) || !test(a, d, e) || !test(b, c, e) || !test(b, d, e) || !test(c, d, e)
+                            || !test(a, b, c, e) || !test(a, b, d, e) || !test(a, c, d, e) || !test(b, c, d, e)
+                        ) { continue; }
+
+                        //show(a); show(b); show(c); show(d); show(e);
+                        //cout << endl;
 
                         for (int f = e+1; f < size; ++f) {
-                            if (!test(f, e) || !test(f, d) || !test(f, c) || !test(f, b) || !test(f, a)) { continue; }
+                            if (!test(f, e) || !test(f, d) || !test(f, c) || !test(f, b) || !test(f, a)
+                                || !test(a, b, f) || !test(a, c, f) || !test(a, d, f) || !test(a, e, f) || !test(b, c, f) || !test(b, d, f) || !test(b, e, f)
+                                || !test(c, d, f) || !test(c, e, f) || !test(d, e, f)
+                                || !test(a, b, c, f) || !test(a, b, d, f) || !test(a, b, e, f) || !test(a, c, d, f) || !test(a, c, e, f) || !test(a, d, e, f)
+                                || !test(b, c, d, f) || !test(b, c, e, f) || !test(b, d, e, f) || !test(c, d, e, f)
+                            ) { continue; }
                             show(a); show(b); show(c); show(d); show(e); show(f);
                             cout << endl;
 
                             for (int g = f+1; g < size; ++g) {
-                                if (!test(g, f) || !test(g, e) || !test(g, d) || !test(g, c) || !test(g, b) || !test(g, a)) { continue; }
+                                if (!test(g, f) || !test(g, e) || !test(g, d) || !test(g, c) || !test(g, b) || !test(g, a)
+                                    || !test(a, b, g) || !test(a, c, g) || !test(a, d, g) || !test(a, e, g) || !test(a, f, g)
+                                    || !test(b, c, g) || !test(b, d, g) || !test(b, e, g) || !test(b, f, g)
+                                    || !test(c, d, g) || !test(c, e, g) || !test(c, f, g) || !test(d, e, g) || !test(d, f, g)  || !test(e, f, g)
+                                    || !test(a, b, c, g) || !test(a, b, d, g) || !test(a, b, e, g) || !test(a, b, f, g) || !test(a, c, d, g) || !test(a, c, e, g) || !test(a, c, f, g)
+                                    || !test(a, d, e, g) || !test(a, d, f, g) || !test(a, e, f, g)
+                                    || !test(b, c, d, g) || !test(b, c, e, g) || !test(b, c, f, g) || !test(b, d, e, g) || !test(b, d, f, g) || !test(b, e, f, g)
+                                    || !test(c, d, e, g) || !test(c, d, f, g) || !test(c, e, f, g) || !test(d, e, f, g)
+                                ) { continue; }
                                 cout << "!!!!!!!!!!!!!!!!!!!!!!\n";
                                 show(a); show(b); show(c); show(d); show(e); show(f); show(g);
                                 cout << endl;
                                 return;
-
-                                for (int h = g+1; h < size; ++h) {
-                                    if (!test(h, g) || !test(h, f) || !test(h, e) || !test(h, d) || !test(h, c) || !test(h, b) || !test(h, a)) { continue; }
-                                    show(a); show(b); show(c); show(d); show(e); show(f); show(g); show(h);
-                                    cout << endl;
-
-                                    for (int i = h+1; i < size; ++i) {
-                                        if (!test(i, h) || !test(i, g) || !test(i, f) || !test(i, e) || !test(i, d) || !test(i, c) || !test(i, b) || !test(i, a)) { continue; }
-                                        cout << "######################\n";
-                                        show(a); show(b); show(c); show(d); show(e); show(f); show(g); show(h); show(i);
-                                        cout << endl;
-
-                                        for (int j = i+1; j < size; ++j) {
-                                            if (!test(j, i) || !test(j, h) || !test(j, g) || !test(j, f) || !test(j, e) || !test(j, d) || !test(j, c) || !test(j, b) || !test(j, a)) { continue; }
-                                            cout << "######################\n";
-                                            show(a); show(b); show(c); show(d); show(e); show(f); show(g); show(h); show(i); show(j);
-                                            cout << endl;
-                                        }
-
-                                    }
-
-                                }
                             }
                         }
                     }
                 }
             }
-            //show(a); show(b);
-            //cout << endl;
         }
     }
 }
 
 void found(bit64_t a) {
-    if (!test_d(10, a, a)) { return; }
-    if (!test_d(13, a, a, a)) { return; }
-    if (!test_d(18, a, a, a, a)) { return; }
-    deBruijn.push_back(a);
-    //cout << right << setfill(' ') /*<< setw(8) << dec << i*/ << " 0x" << setfill('0') << hex << setw(16) << a << "ull,\n" << std::flush;
+    if (!test_d(9, a, a)) { return; }
+    if (!test_d(12, a, a, a)) { return; }
+    if (!test_d(20, a, a, a, a)) { return; }
+    //deBruijn.push_back(a);
+    cout /*<< right << setfill(' ') << setw(8) << dec << deBruijn.size()*/ << " 0x" << setfill('0') << hex << setw(16) << a << "ull,\n" << std::flush;
 }
 
 void findDeBruijn(U64 seq, int depth, int vtx, int nz) {
@@ -282,7 +279,7 @@ void findDeBruijn(U64 seq, int depth, int vtx, int nz) {
 
 void run() {
     for (int i=0; i < 64; i++) { pow2[i] = (U64)1 << i; }
-    findDeBruijn(0, 64-6, 0, 6);
+    //findDeBruijn(0, 64-6, 0, 6);
     cout << dec << deBruijn.size() << endl;
     findCombi();
 }
@@ -304,14 +301,32 @@ int main(int, const char** ) {
 //0x03ca242d98d3bf57ull,
 
 //10/32-1/8-9/32-1/9; 8/32-1/11 8/32-1/14-8/32-1/14
-0x0218a392cd5d3dbfull,
-0x024530decb9f8eadull,
-0x02b91efc4b53a1b3ull,
-0x02dc61d5ecfc9a51ull,
-0x031faf09dcda2ca9ull,
-0x0352138afdd1e65bull,
-0x03ac4dfb48546797ull,
-     };
+//0x0218a392cd5d3dbfull,
+//0x024530decb9f8eadull,
+//0x02b91efc4b53a1b3ull,
+//0x02dc61d5ecfc9a51ull,
+//0x031faf09dcda2ca9ull,
+//0x0352138afdd1e65bull,
+//0x03ac4dfb48546797ull,
+
+//9/12 13/19
+0x021937e7b52c7457ull,
+0x022fcddab271e943ull,
+0x026cf7e2ba396a43ull,
+0x02c4f74a873236bfull,
+0x02d87e7195eea44dull,
+0x0334f238a12dd5fbull,
+0x03cbbf58da122a67ull,
+
+//9/12/20 14/20/28
+0x0219bf251c5a7abbull,
+0x0230e2a4d67e5eddull,
+0x026e7b57e3a1948bull,
+0x031cd422b765fa4full,
+0x03c4efd5c8cda50bull,
+0x03c85a312bf66ea7ull,
+0x03ca91b316bf4277ull,
+          };
 
     std::mt19937_64 random;
     bit64_t zobrist[2][7][64];
