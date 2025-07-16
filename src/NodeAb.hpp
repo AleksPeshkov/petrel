d4fb856f4c4db8eba98e24056f0038247710d4ee
Aleks Peshkov <aleks.peshkov@gmail.com>
2025-07-16 20:36:19 +0300

    struct Score

--- src/NodeAb.hpp
+++ src/NodeAb.hpp
@@ -11,14 +11,18 @@ protected:
     ply_t ply = 0; //distance from root
     ply_t draft = 0; //remaining depth
 
-    Score score = Score::None;
-    Score alpha = Score::Minimum;
-    Score beta = Score::Maximum;
+    Score score = NoScore;
+    Score alpha = MinusInfinity;
+    Score beta = PlusInfinity;
 
     NodeAb (const PositionMoves& p, SearchControl& c) : Node{p, c}, parent{*this} {}
 
+    Score staticEval();
     NodeControl visit(Move);
     NodeControl negamax(Score, Move);
+
+    Move createFullMove(Move move) const { return createFullMove(move.from(), move.to()); }
+    Move createFullMove(Square from, Square to) const;
 
 public:
     NodeAb (NodeAb& n) : Node{n.control}, parent{n}, ply{n.ply + 1} {}
