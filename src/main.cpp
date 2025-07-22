d6d1b2db936588edfddaf266c44dcfaa6c3e999d
Aleks Peshkov <aleks.peshkov@gmail.com>
2025-07-22 13:31:25 +0300

    refactoring Zobrist

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,6 @@
 #include "SquaresInBetween.hpp"
 #include "Uci.hpp"
 #include "VectorOfAll.hpp"
-#include "Zobrist.hpp"
 
 /**
 * Startup constant initialization
@@ -25,7 +24,6 @@ constexpr const VectorOfAll vectorOfAll; //4k 256*16
 const PieceSquareTable pieceSquareTable; //3k 6*64*8
 constexpr const PiSingle piSingle; //256
 const CastlingRules castlingRules; //128
-constexpr const ZobristKey zobristKey; //64 8*8
 constexpr const BitReverse bitReverse; //32
 
 template <> io::czstring PieceType::The_string{"qrbnpk"};
