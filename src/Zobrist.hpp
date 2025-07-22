9922b5ab9724593d570d424329032f4c5546b600
Aleks Peshkov <aleks.peshkov@gmail.com>
2025-07-22 13:31:25 +0300

    refactoring Zobrist

--- src/Zobrist.hpp
+++ src/Zobrist.hpp
@@ -2,33 +2,77 @@
 #define ZOBRIST_HPP
 
 #include "bitops.hpp"
-#include "ZobristKey.hpp"
+#include "io.hpp"
+#include "typedefs.hpp"
 
-class Zobrist {
-    typedef ZobristKey::Index Index;
-
+class Z {
 public:
-    typedef ZobristKey::_t _t;
-    typedef Zobrist Arg;
+    typedef const Z& Arg;
+    typedef u64_t _t;
 
-private:
+protected:
     _t v;
-    void change(Index ty, Square to) { v ^= ::zobristKey(ty, to); }
 
 public:
-    constexpr explicit Zobrist (_t z) : v{z} {}
+    constexpr Z(_t n = 0) : v{n} {}
     constexpr operator const _t& () const { return v; }
 
-    Zobrist (Arg my, Arg op) : v{ my.v ^ ::byteswap(op.v) } {}
+    constexpr Z& flip() { v = ::byteswap(v); return *this; }
+    constexpr Z operator ~ () const { return Z{*this}.flip(); }
 
-    void drop(PieceType ty, Square to) { change(Index{ty}, to); }
-    void clear(PieceType ty, Square from) { drop(ty, from); }
+    constexpr Z& operator ^= (Arg z) { v ^= z.v; return *this; }
+    constexpr friend Z operator ^ (Arg a, Arg b) { return Z{a} ^= b; }
 
-    void setCastling(Square sq)  { assert (sq.on(Rank1)); change(ZobristCastling, sq); }
+    constexpr friend bool operator == (Arg a, Arg b) { return a.v == b.v; }
+    constexpr friend bool operator != (Arg a, Arg b) { return a.v != b.v; }
+
+    friend out::ostream& operator << (out::ostream& out, Z z) {
+        auto flags = out.flags();
+
+        out << " [" << std::hex << std::setw(16) << std::setfill('0') << "]";
+        out << static_cast<Z::_t>(z);
+
+        out.flags(flags);
+        return out;
+    }
+};
+
+class Zobrist : public Z {
+    typedef PieceZobristType Index;
+
+protected:
+
+    //hand picked set of de Bruijn numbers
+    enum : _t {
+        _Queen  = ULL(0x0218a392cd5d3dbf),
+        _Rook   = ULL(0x024530decb9f8ead),
+        _Bishop = ULL(0x02b91efc4b53a1b3),
+        _Knight = ULL(0x02dc61d5ecfc9a51),
+        _Pawn   = ULL(0x031faf09dcda2ca9),
+        _King   = ULL(0x0352138afdd1e65b),
+        _Extra  = ULL(0x03ac4dfb48546797),
+        _Castling = _Extra ^ _Rook,
+        _EnPassant = _Extra ^ _Pawn,
+    };
+
+    constexpr static const Index::arrayOf<_t> key = {{
+        _Queen, _Rook, _Bishop, _Knight, _Pawn, _King, _Castling, _EnPassant
+    }};
+
+public:
+    using Z::Z;
+    Zobrist (Arg my, Arg op) : Z{my ^ ~op} {}
+
+    void change(Index ty, Square to) { *this ^= ::rotateleft(key[ty], to); }
+
+    void drop(PieceType ty, Square to) { change(static_cast<Index>(ty), to); }
+    void clear(PieceType ty, Square from) { change(static_cast<Index>(ty), from); }
+
+    void setCastling(Square sq)  { assert (sq.on(Rank1)); change(Castling, sq); }
     void clearCastling(Square sq) { setCastling(sq); }
 
-    void setEnPassant(Square sq) { assert (sq.on(Rank4)); change(ZobristEnPassant, sq); }
-    void setEnPassant(File fileFrom) { setEnPassant(Square{fileFrom, Rank4}); }
+    void setEnPassant(Square sq) { assert (sq.on(Rank4)); change(EnPassant, sq); }
+    void setEnPassant(File file) { setEnPassant(Square{file, Rank4}); }
     void clearEnPassant(Square sq) { setEnPassant(sq); }
 
     void move(PieceType ty, Square from, Square to) {
@@ -41,18 +85,6 @@ public:
         assert (from.on(Rank7) && to.on(Rank8));
         clear(Pawn, from);
         drop(ty, to);
-    }
-
-    constexpr friend bool operator == (Arg a, Arg b) { return a.v == b.v; }
-
-    friend io::ostream& operator << (io::ostream& out, Zobrist z) {
-        auto flags(out.flags());
-
-        out << std::hex << std::setw(16) << std::setfill('0');
-        out << static_cast<Zobrist::_t>(z);
-
-        out.flags(flags);
-        return out;
     }
 
 };
