ec006a22f6fe66be8ff783c4b80fb5a5cf00f4d0
Aleks Peshkov <aleks.peshkov@gmail.com>
2025-07-23 11:41:19 +0300

    PositionMove::zobrist => Position::zobrist, Position::makeMoveNoZobrist()

--- src/PositionMoves.hpp
+++ src/PositionMoves.hpp
@@ -13,9 +13,6 @@ protected:
 private:
     Bb attackedSquares; //squares attacked by all opponent pieces
 
-    Zobrist zobrist{0};
-    index_t _movesCount = 0;
-
     //legal move generation helpers
     template <Side::_t> void excludePinnedMoves(PiMask);
     template <Side::_t> void correctCheckEvasionsByPawns(Bb, Square);
@@ -27,30 +24,24 @@ private:
     template <Side::_t> void generateCheckEvasions();
     template <Side::_t> void generateMoves();
 
-    Zobrist createZobrist(Square, Square) const;
-    Zobrist generateZobrist() const;
-
 protected:
+
     void makeMove(Square, Square);
-    void makeMove(PositionMoves& parent, Square, Square);
+    void makeMove(PositionMoves& parent, Square from, Square to);
+    void makeMoveNoZobrist(PositionMoves& parent, Square from, Square to);
+
     void makeMove(Move move) { return makeMove(move.from(), move.to()); }
     void makeMove(PositionMoves& parent, Move move) { return makeMove(parent, move.from(), move.to()); }
 
-    void generateMoves();
+    void makeMoves();
     bool isLegalMove(Move move) const { return move && isLegalMove(move.from(), move.to()); }
     bool isLegalMove(Square from, Square to) const;
     void clearMove(Move move);
     void clearMove(Pi pi, Square to);
-    void setMoves(const PiBb& m, index_t n);
 
     bool inCheck() const;
 
-    void setZobrist(const PositionMoves& parent, Square from, Square to) { zobrist = parent.createZobrist(from, to); }
-    void setZobrist() { zobrist = generateZobrist(); }
-
-    const Zobrist& getZobrist() const { return zobrist; }
-    index_t movesCount() const { return _movesCount; }
-    PiBb cloneMoves() const { return PiBb{moves}; }
+    bool isOpAttacks(Square sq) const { return attackedSquares.has(sq); }
 };
 
 #endif
