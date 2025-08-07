5ab26f80a617fa91e65be4d2affbedaaf8e4e6b9
Aleks Peshkov <aleks.peshkov@gmail.com>
2025-08-07 11:56:19 +0300

    perft divide as default

--- src/NodePerft.hpp
+++ src/NodePerft.hpp
@@ -6,20 +6,19 @@
 class NodeRoot;
 
 class NodePerft : public PositionMoves {
-    NodeRoot& root;
     NodePerft* const parent;
-    Ply draft;
+    NodeRoot& root;
+
     node_count_t perft = 0;
+    Ply draft;
 
-    NodePerft (NodePerft* n) : root{n->root}, parent{n}, draft{n->draft-1} {}
-
-    ReturnStatus visit(Square from, Square to);
-    ReturnStatus searchMoves();
-    ReturnStatus searchDivide();
+    NodePerft (NodePerft* n) : parent{n}, root{n->root}, draft{n->draft-1} {}
+    ReturnStatus visit();
+    ReturnStatus visitMove(Square from, Square to);
 
 public:
-    NodePerft (NodeRoot& r, Ply d);
-    void visitRoot(bool isDivide);
+    NodePerft (NodeRoot&, Ply);
+    ReturnStatus visitRoot();
 };
 
 #endif
