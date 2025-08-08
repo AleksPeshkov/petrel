6c31fdf5f8866cd78082c82e1507446fa2a8ad3a
Aleks Peshkov <aleks.peshkov@gmail.com>
2025-08-08 15:36:51 +0300

    Uci::next() => Uci::consume()

--- src/Node.cpp
+++ src/Node.cpp
@@ -42,7 +42,7 @@ ReturnStatus Node::searchRoot() {
         alpha = MinusInfinity;
         beta = PlusInfinity;
         RETURN_IF_STOP (search());
-        root.uci.infoIterationEnd(draft);
+        root.uci.info_iteration(draft);
         root.newIteration();
     }
 
@@ -120,7 +120,7 @@ ReturnStatus Node::negamax(Node* child) {
             ++root.tt.writes;
 
             if (ply == 0) {
-                root.uci.infoNewPv(draft, score);
+                root.uci.info_pv(draft, score);
             }
         }
     }
