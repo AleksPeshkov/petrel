34d24f1bae7a07162d561d228dd31508d62ea465
Aleks Peshkov <aleks.peshkov@gmail.com>
2025-08-18 21:22:15 +0300

    move Node::movesMade => PositionMoves::movesMade

--- src/PositionMoves.hpp
+++ src/PositionMoves.hpp
@@ -8,8 +8,9 @@
 class PositionMoves : public Position {
     PiBbMatrix moves_; // generated strictly legal moves
 
-    Bb bbAttacked_; // bitboard of squares attacked by any opponent piece
-    bool inCheck_; // king of current side to move is under attack
+    Bb bbAttacked_; // bitboard of squares attacked by any opponent (not side to move) piece (set during moves generation)
+    MovesNumber movesMade_; // number of moves already made in this node (set to 0 during moves generation)
+    bool inCheck_; // king of current side to move is under attack (set during moves generation)
 
     //legal move generation helpers
     template <Side::_t> void excludePinnedMoves(PiMask);
@@ -23,7 +24,8 @@ class PositionMoves : public Position {
     template <Side::_t> void generateMoves();
 
 protected:
-    void setMoves(const PiBbMatrix& moves) { moves_ = moves; }
+    void setMoves(const PiBbMatrix& moves) { moves_ = moves; movesMade_ = 0; }
+    void clearMove(Square from, Square to) { moves_.clear(MY.pieceAt(from), to); ++movesMade_; }
 
     void makeMove(Square, Square);
     void makeMove(PositionMoves* parent, Square from, Square to);
@@ -32,29 +34,36 @@ protected:
     void makeMove(Move move) { return makeMove(move.from(), move.to()); }
     void makeMove(PositionMoves* parent, Move move) { return makeMove(parent, move.from(), move.to()); }
 
-    void makeMoves();
+    void generateMoves();
 
 public:
-    // strictly legal moves not yet searched
+    // strictly legal moves not yet made
     constexpr const PiBbMatrix& moves() const { return moves_; }
 
-    // bitboard of squares attacked by any opponent piece
+    // number of moves have been already made (and have already cleared from the moves matrix))
+    constexpr const MovesNumber& movesMade() const { return movesMade_; }
+
+    // check if the given move is legal and not yet made
+    bool isLegalMove(Move move) const {
+        return move && isLegalMove(move.from(), move.to());
+    }
+
+    // check if the given move is legal and not yet made
+    bool isLegalMove(Square from, Square to) const {
+        return MY.has(from) && moves_.has(MY.pieceAt(from), to);
+    }
+
+    // bitboard of squares attacked by any opponent (not side to move) piece (set during moves generation)
     constexpr const Bb& bbAttacked() const { return bbAttacked_; }
 
     // king of current side to move is under attack
     constexpr const bool& inCheck() const { return inCheck_; }
 
-    // bitboard of all moves of the given piece not yet searched
+    // bitboard of all moves of the given piece not yet made
     constexpr Bb movesOf(Pi pi) const { return moves_[pi]; }
 
-    // pieces that have a not yet searched legal move to the given square
-    constexpr PiMask canMoveTo(Square sq) const { return moves_[sq]; }
-
-    // check if the given move is legal and not yet searched
-    bool isLegalMove(Move move) const { return move && isLegalMove(move.from(), move.to()); }
-
-    // check if the given move is legal and not yet searched
-    bool isLegalMove(Square from, Square to) const;
+    // pieces that have a not yet made legal move to the given square
+    PiMask canMoveTo(Square sq) const { return moves_[sq]; }
 };
 
 #endif
