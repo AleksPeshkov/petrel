77a61ff9737332a1074d2d5c046d475c171840f2
Aleks Peshkov <aleks.peshkov@gmail.com>
2025-08-18 18:40:25 +0300

    rename refactorings of Position, PositionSide and force data members be private

--- src/Position.hpp
+++ src/Position.hpp
@@ -5,22 +5,23 @@
 #include "Zobrist.hpp"
 
 // side to move
-#define MY ((*this)[My])
+#define MY positionSide(My)
 
 // opponent side
-#define OP ((*this)[Op])
+#define OP positionSide(Op)
+
+// all occupied squares by both sides from the current side point of view
+#define OCCUPIED occupied(My)
 
 class Position {
-    Side::arrayOf<PositionSide> positionSide; //copied from the parent, updated incrementally
-    Side::arrayOf<Bb> occupiedBb; // both color pieces combined, recalculated after each move
+    Side::arrayOf<PositionSide> positionSide_; //copied from the parent, updated incrementally
+    Side::arrayOf<Bb> occupied_; // both color pieces combined, updated from positionSide[] after each move
 
-protected:
-    Zobrist zobrist; // incrementally updated position hash
-    Square lastPieceFrom; // last moved piece (rook in case of castling) initial square
-    Square lastPieceTo; // last moved piece actual destination square (rook square in case of castling or en passant capture pawn square)
-    Rule50 rule50; // number of irreversible moves before, incremented or reset by makeMove()
+    Zobrist zobrist_; // incrementally updated position hash
+    Square movedPieceFrom_; // last moved piece (rook in case of castling) initial square
+    Square movedPieceTo_; // last moved piece actual destination square (rook square in case of castling or en passant capture pawn square)
+    Rule50 rule50_; // number of halfmoves since last capture or pawn move, incremented or reset by makeMove()
 
-private:
     template <Side::_t> void updateSliderAttacks(PiMask);
     template <Side::_t> void updateSliderAttacks(PiMask, PiMask);
 
@@ -29,34 +30,55 @@ private:
 
     template <Side::_t> Zobrist generateZobrist() const;
 
-    //calculate Zobrist key from scratch
+    // calculate Zobrist key from scratch
     Zobrist createZobrist(Square, Square) const;
+    Zobrist generateZobrist() const;
 
-public:
-    constexpr PositionSide& operator[] (Side side) { return positionSide[side]; }
-    constexpr const PositionSide& operator[] (Side side) const { return positionSide[side]; }
+protected:
+    constexpr PositionSide& positionSide(Side side) { return positionSide_[side]; }
 
-    template <Side::_t My> constexpr const Bb& occupied() const { return occupiedBb[My]; }
+    // update the zobrist hash of incoming move
+    void makeZobrist(const Position* parent, Square from, Square to) { zobrist_ = parent->createZobrist(from, to); }
 
-    Score evaluate() const { return PositionSide::evaluate(MY, OP); }
-
-    // 1st step of making a move: update the zobrist hash
-    void makeZobrist(const Position* parent, Square from, Square to) { zobrist = parent->createZobrist(from, to); }
-
-    // 2nd step of making a move: update the position (including attack matrix)
+    // update the position without updating the zobrist hash
     void makeMoveNoZobrist(const Position*, Square, Square);
 
-    // 1st and 2nd step combined
+    // update the position and its zobrist hash
     void makeMove(const Position*, Square, Square);
 
+    // make move directly inside position itself
     void makeMove(Square, Square);
+
+    template <Side::_t> void setLegalEnPassant(Pi, Square);
+    void setZobrist() { zobrist_ = generateZobrist(); }
+    void setRule50(const Rule50& rule50) { rule50_ = rule50; }
+
+public:
+    constexpr const PositionSide& positionSide(Side side) const { return positionSide_[side]; }
+
+    // all occupied squares by both sides from the given side point of view
+    constexpr const Bb& occupied(Side side) const { return occupied_[side]; }
+
+    // position hash
+    constexpr const Zobrist& zobrist() const { return zobrist_; }
+
+    // last moved piece from square (differs from last move.from() in case of special move)
+    constexpr Square movedPieceFrom() const { return movedPieceFrom_; }
+
+    // last moved piece to square (differs from last move.to() in case of special move)
+    constexpr Square movedPieceTo() const { return movedPieceTo_; }
+
+    // number of halfmoves since last capture or pawn move
+    constexpr const Rule50& rule50() const { return rule50_; }
+
+    Score evaluate() const { return Evaluation::evaluate(MY.evaluation(), OP.evaluation()); }
+
     bool isSpecial(Square, Square) const;
 
-    //initial position setup
+// initial position setup
+
     bool dropValid(Side, PieceType, Square);
     bool afterDrop();
-    template <Side::_t> void setLegalEnPassant(Pi, Square);
-    Zobrist generateZobrist() const;
 };
 
 #endif
