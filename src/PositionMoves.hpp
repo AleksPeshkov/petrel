5b6834bcc24a9cad492c9eab1fd2a1d426701da5
Aleks Peshkov <aleks.peshkov@gmail.com>
2025-08-18 21:03:29 +0300

    rename refactoring of PositionMoves ans force data member to be private

--- src/PositionMoves.hpp
+++ src/PositionMoves.hpp
@@ -6,14 +6,11 @@
 #include "Zobrist.hpp"
 
 class PositionMoves : public Position {
-public:
-    PiBbMatrix moves; // generated legal only moves
+    PiBbMatrix moves_; // generated strictly legal moves
 
-protected:
-    Bb attackedSquares; //squares attacked by all opponent pieces
-    bool inCheck;
+    Bb bbAttacked_; // bitboard of squares attacked by any opponent piece
+    bool inCheck_; // king of current side to move is under attack
 
-private:
     //legal move generation helpers
     template <Side::_t> void excludePinnedMoves(PiMask);
     template <Side::_t> void correctCheckEvasionsByPawns(Bb, Square);
@@ -26,6 +23,8 @@ private:
     template <Side::_t> void generateMoves();
 
 protected:
+    void setMoves(const PiBbMatrix& moves) { moves_ = moves; }
+
     void makeMove(Square, Square);
     void makeMove(PositionMoves* parent, Square from, Square to);
     void makeMoveNoZobrist(PositionMoves* parent, Square from, Square to);
@@ -34,7 +33,27 @@ protected:
     void makeMove(PositionMoves* parent, Move move) { return makeMove(parent, move.from(), move.to()); }
 
     void makeMoves();
+
+public:
+    // strictly legal moves not yet searched
+    constexpr const PiBbMatrix& moves() const { return moves_; }
+
+    // bitboard of squares attacked by any opponent piece
+    constexpr const Bb& bbAttacked() const { return bbAttacked_; }
+
+    // king of current side to move is under attack
+    constexpr const bool& inCheck() const { return inCheck_; }
+
+    // bitboard of all moves of the given piece not yet searched
+    constexpr Bb movesOf(Pi pi) const { return moves_[pi]; }
+
+    // pieces that have a not yet searched legal move to the given square
+    constexpr PiMask canMoveTo(Square sq) const { return moves_[sq]; }
+
+    // check if the given move is legal and not yet searched
     bool isLegalMove(Move move) const { return move && isLegalMove(move.from(), move.to()); }
+
+    // check if the given move is legal and not yet searched
     bool isLegalMove(Square from, Square to) const;
 };
 
