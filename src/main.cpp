646d2d548c0a597280715ef6679865ba248062e9
Aleks Peshkov <aleks.peshkov@gmail.com>
2025-08-20 13:09:11 +0300

    remove AttacksFrom.hpp, CastlingRules.hpp, PvMoves.hpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,7 @@
 #include <errno.h>
 #include "assert.hpp"
 #include "io.hpp"
-#include "AttacksFrom.hpp"
 #include "Bb.hpp"
-#include "CastlingRules.hpp"
 #include "Evaluation.hpp"
 #include "Hyperbola.hpp"
 #include "PiMask.hpp"
@@ -13,15 +11,15 @@
 /**
 * Startup constant initialization
 */
-const InBetween inBetween; //32k 64*64*8
-const HyperbolaDir hyperbolaDir; //4k 64*4*16
-const HyperbolaSq hyperbolaSq; //1k 64*16
-const AttacksFrom attacksFrom; //3k 6*64*8
-constexpr const VectorOfAll vectorOfAll; //4k 256*16, used by bitReverse initialization
-const PieceSquareTable pieceSquareTable; //3k 6*64*8
-constexpr const PiSingle piSingle; //256
-const CastlingRules castlingRules; //128
-constexpr const BitReverse bitReverse; //32
+const InBetween inBetween; // 32k 64*64*8
+const HyperbolaDir hyperbolaDir; // 4k 64*4*16
+const HyperbolaSq hyperbolaSq; // 1k 64*16
+const AttacksFrom attacksFrom; // 3k 6*64*8
+constexpr const VectorOfAll vectorOfAll; // 4k 256*16, used by bitReverse initialization
+const PieceSquareTable pieceSquareTable; // 3k 6*64*8
+constexpr const PiSingle piSingle; // 256
+const CastlingRules castlingRules; // 128
+constexpr const BitReverse bitReverse; // 32
 
 template <> io::czstring PieceType::The_string{"qrbnpk"};
 template <> io::czstring PromoType::The_string{"qrbn"};
