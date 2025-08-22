5dc7fb0d6b348e9417f4cffa73afd959c7782612
Aleks Peshkov <aleks.peshkov@gmail.com>
2025-08-22 15:52:47 +0300

    improved SEE; goodCaptures(), badCaptures()

--- src/Node.hpp
+++ src/Node.hpp
@@ -81,16 +81,11 @@ protected:
     [[nodiscard]] ReturnStatus searchMoves();
     [[nodiscard]] ReturnStatus quiescence();
 
-    [[nodiscard]] ReturnStatus goodCaptures(Node*);
-    [[nodiscard]] ReturnStatus notBadCaptures(Node*);
-    [[nodiscard]] ReturnStatus allCaptures(Node*);
+    // promotions to queen, winning or equal captures, also uncertain by current SEE captures
+    [[nodiscard]] ReturnStatus goodCaptures(Node*, const PiMask&);
 
-    [[nodiscard]] ReturnStatus goodCaptures(Node*, Square);
-    [[nodiscard]] ReturnStatus notBadCaptures(Node*, Square);
-    [[nodiscard]] ReturnStatus allCaptures(Node*, Square);
-
-    [[nodiscard]] ReturnStatus safePromotions(Node*);
-    [[nodiscard]] ReturnStatus allPromotions(Node*);
+    // remaining (bad) captures and all underpromotions
+    [[nodiscard]] ReturnStatus badCaptures(Node*, const PiMask&);
 
     UciMove uciMove(Move move) const { return uciMove(move.from(), move.to()); }
     UciMove uciMove(Square from, Square to) const;
