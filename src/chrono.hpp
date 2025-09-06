789db9e62066024488a5c31bea76c87d630e40da
Aleks Peshkov <aleks.peshkov@gmail.com>
2025-09-07 00:30:05 +0300

    simplified time management logic

--- src/chrono.hpp
+++ src/chrono.hpp
@@ -2,13 +2,13 @@
 #define CHRONO_HPP
 
 #include <chrono>
-using std::chrono::duration_cast;
-using std::chrono::milliseconds;
+#include "io.hpp"
+
 using namespace std::chrono_literals;
 
-typedef std::chrono::steady_clock clock_type;
-typedef clock_type::time_point TimePoint;
-typedef clock_type::duration TimeInterval;
+using clock_type = std::chrono::steady_clock;
+using TimePoint = clock_type::time_point;
+using TimeInterval = clock_type::duration;
 
 inline TimePoint timeNow() {
     return clock_type::now();
@@ -22,6 +22,23 @@ inline TimeInterval elapsedSince(TimePoint start) {
 // ::timeNow() + timeInterval
 inline TimePoint timeIn(TimeInterval timeInterval) {
     return ::timeNow() + timeInterval;
+}
+
+inline istream& operator >> (istream& in, TimeInterval& timeInterval) {
+    int msecs;
+    if (in >> msecs) {
+        timeInterval = std::chrono::duration_cast<TimeInterval>(std::chrono::milliseconds{msecs} );
+    }
+    return in;
+}
+
+inline ostream& operator << (ostream& out, const TimeInterval& timeInterval) {
+    return out << std::chrono::duration_cast<std::chrono::milliseconds>(timeInterval).count();
+}
+
+template <typename nodes_type, typename duration_type>
+constexpr nodes_type nps(nodes_type nodes, duration_type duration) {
+    return (nodes * duration_type::period::den) / (static_cast<nodes_type>(duration.count()) * duration_type::period::num);
 }
 
 #endif
