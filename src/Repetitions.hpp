af140e018000102c7929310c659f54fb853d0749
Aleks Peshkov <aleks.peshkov@gmail.com>
2025-09-30 18:37:34 +0300

    test for 2nd repetition in history; make unit; TestRepetitions.hpp

--- src/Repetitions.hpp
+++ src/Repetitions.hpp
@@ -4,138 +4,143 @@
 #include "typedefs.hpp"
 #include "Zobrist.hpp"
 
-class RepetitionMask {
+// https://www.talkchess.com/forum/viewtopic.php?p=554664#p554664
+class RepetitionHash {
     using _t = u64_t;
     _t v{0};
 
-    static constexpr _t mask(Z z) { return ::singleton<_t>(z & 077); }
+    static constexpr _t hash(Z z) { return ::singleton<_t>(z & 077); }
 public:
-    constexpr RepetitionMask () : v{0} {}
-    constexpr RepetitionMask (const RepetitionMask& m, Z z) : v{m.v | mask(z)} {}
-    constexpr bool has(Z z) const { return (v & mask(z)) != 0; }
+    constexpr RepetitionHash () : v{0} {}
+    constexpr RepetitionHash (const RepetitionHash& m, Z z) : v{m.v | hash(z)} {}
+    constexpr bool has(Z z) const { return (v & hash(z)) != 0; }
+};
+
+class RepetitionsSide {
+    using RepIndex = Index<50>;
+
+    struct RepEntry {
+        Z z;
+        RepetitionHash hash;
+    };
+
+    constexpr static RepIndex Last{RepIndex::Last};
+
+    RepIndex::arrayOf<RepEntry> reps;
+    int count = 0; // number of entries
+    RepIndex last{RepIndex::Last}; // last added entry index
+
+public:
+    constexpr RepetitionsSide () { reps[last].hash = {}; }
+
+    constexpr void clear() {
+        last = Last;
+        count = 0;
+    }
+
+    constexpr void push(Z z) {
+        auto prevHash{(count == 0) ? RepetitionHash{} : RepetitionHash{reps[last].hash, reps[last].z}};
+        last = RepIndex{last < Last ? last+1 : 0};
+        reps[last].z = z;
+        reps[last].hash = prevHash;
+        count = count < RepIndex::Size ? count+1 : count;
+    }
+
+    constexpr void dropLast() {
+        if (count == 0) { assert (false); return; }
+        last  = last > 0 ? RepIndex{last-1} : Last;
+        count = count-1;
+    }
+
+    constexpr void normalize() {
+        if (count <= 1) { return; }
+
+        if (count != RepIndex::Size) {
+            // just reverse order
+            for (int i = 0; i < count/2; ++i) {
+                std::swap(reps[i], reps[last - i]);
+            }
+            return;
+        }
+
+        // rare buffer overflow case
+
+        RepIndex::arrayOf<Z> temp;
+        auto current = last < RepIndex::Last ? last+1 : 0;
+
+        // copy z elements to the beginning
+        for (int i = 0; i < count; ++i) {
+            temp[i] = reps[current].z;
+            current = current < Last ? current+1 : 0;
+        }
+
+        RepetitionHash hash{};
+
+        // push again in reverse order
+        for (int i = 0; i < count; ++i) {
+            auto z = temp[i];
+            hash = {hash, z};
+            reps[count - i - 1].z = z;
+            reps[count - i - 1].hash = hash;
+        }
+
+        count = 0;
+        last = Last;
+    }
+
+    constexpr bool has(Z z) const {
+        int repetitions = 0;
+        RepIndex i{0};
+        while (true) {
+            if (z == reps[i].z) {
+                ++repetitions;
+                if (repetitions == 2) {
+                    return true;
+                }
+            }
+            if (!reps[i].hash.has(z)) {
+                return false;
+            }
+            ++i;
+        }
+    }
+
+    constexpr RepetitionHash repetitionHash() const {
+        return (count == 0) ?  RepetitionHash{} : RepetitionHash{reps[0].hash, reps[0].z};
+    }
+
+    // used for unit testing
+    constexpr auto size() const { return count; }
 };
 
 class Repetitions {
-    class RepRootSide {
-        using RepIndex = Index<50>;
-
-        struct RepEntry {
-            Z z;
-            RepetitionMask mask;
-        };
-
-        constexpr static RepIndex Last{RepIndex::Last};
-
-        RepIndex::arrayOf<RepEntry> reps;
-        int count = 0; // number of entries
-        RepIndex last{RepIndex::Last}; // last added entry index
-
-    public:
-        constexpr RepRootSide () { reps[last].mask = {}; }
-
-        constexpr void clear() {
-            count = 0;
-            last = Last;
-            reps[last].mask = {};
-        }
-
-        constexpr void dropLast() {
-            if (count == 0) { assert (false); return; }
-            count = count-1;
-            last  = last > 0 ? RepIndex{last-1} : Last;
-        }
-
-        void push(Z z) {
-            RepetitionMask mask{reps[last].mask, reps[last].z};
-            last  = RepIndex{last < Last ? last+1 : 0};
-            count = count < RepIndex::Size ? count+1 : count;
-            reps[last].z = z;
-            reps[last].mask = (count == 1) ? RepetitionMask{} :  mask;
-        }
-
-        void normalize() {
-            if (count <= 1) { return; }
-
-            if (count != RepIndex::Size) {
-                // just reverse order
-                for (int i = 0; i < count/2; ++i) {
-                    std::swap(reps[i], reps[last - i]);
-                }
-                return;
-            }
-
-            // rare buffer overflow case
-
-            RepIndex::arrayOf<Z> temp;
-            auto current = last < RepIndex::Last ? last+1 : 0;
-
-            // copy z elements to the beginning
-            for (int i = 0; i < count; ++i) {
-                temp[i] = reps[current].z;
-                current = current < Last ? current+1 : 0;
-            }
-
-            RepetitionMask mask{};
-
-            // push again in reverse order
-            for (int i = 0; i < count; ++i) {
-                auto z = temp[i];
-                mask = {mask, z};
-                reps[count - i - 1].z = z;
-                reps[count - i - 1].mask = mask;
-            }
-
-            count = 0;
-            last = Last;
-        }
-
-        bool has(Z z) const {
-            RepIndex i{0};
-            while (true) {
-                if (z == reps[i].z) {
-                    return true;
-                }
-                if (!reps[i].mask.has(z)) {
-                    return false;
-                }
-                ++i;
-            }
-            return false;
-        }
-
-        RepetitionMask repMask() const {
-            if (count == 0) { return RepetitionMask{}; }
-            return RepetitionMask{reps[0].mask, reps[0].z};
-        }
-    };
-
-    Color::arrayOf<RepRootSide> v;
+    Color::arrayOf<RepetitionsSide> v;
 
 public:
-    void push(Color c, Z z) {
-        return v[c].push(z);
-    }
-
     void clear() {
-        for (auto color : Color::range()) {
-            v[color].clear();
+        for (auto c : Color::range()) {
+            v[c].clear();
         }
     }
 
-    void normalize(Color c) {
-        // the very last position is root and should be removed from history
-        v[c].dropLast();
-        for (auto color : Color::range()) {
-            v[color].normalize();
+    void push(Color color, Z z) {
+        return v[color].push(z);
+    }
+
+    void normalize(Color color) {
+        // the very last position is search root and should be removed from history
+        v[color].dropLast();
+        for (auto c : Color::range()) {
+            v[c].normalize();
         }
     }
 
-    bool has(Color c, Z z) const {
-        return v[c].has(z);
+    bool has(Color color, Z z) const {
+        return v[color].has(z);
     }
 
-    RepetitionMask repMask(Color c) const {
-        return v[c].repMask();
+    RepetitionHash repetitionHash(Color color) const {
+        return v[color].repetitionHash();
     }
 };
 
