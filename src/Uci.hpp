e8d273cd4fac3ed6888ac21a998edfc7c42040c9
Aleks Peshkov <aleks.peshkov@gmail.com>
2025-11-12 15:35:40 +0300

    debug on; fix option Debug Log File

--- src/Uci.hpp
+++ src/Uci.hpp
@@ -291,9 +291,10 @@ private:
     // avoid printing identical 'info nps' lines in a row
     mutable node_count_t lastInfoNodes = 0;
 
-    std::string logFileName;
+    std::string logFileName; // no log by default
     mutable std::ofstream logFile;
     mutable std::mutex logMutex;
+    bool isDebugOn = false;
 
     // try to consume the given token from the inputLine
     bool consume(io::czstring token) { return io::consume(inputLine, token); }
@@ -315,6 +316,7 @@ private:
     void ponderhit();
     void bestmove();
 
+    void debug();
     void goPerft();
     void info_perft_bestmove();
 
