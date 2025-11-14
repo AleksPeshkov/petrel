993ae9fac3352cd88932a0943957eea8b0b797bb
Aleks Peshkov <aleks.peshkov@gmail.com>
2025-11-14 17:36:28 +0300

    uci option EvalFile

--- src/Uci.cpp
+++ src/Uci.cpp
@@ -1,4 +1,5 @@
 #include "chrono.hpp"
+#include "nnue.hpp"
 #include "Uci.hpp"
 #include "UciSearchLimits.hpp"
 #include "Node.hpp"
@@ -25,6 +26,7 @@ Uci::Uci(ostream &o) :
     bestmove_(32, '\0')
 {
     bestmove_.clear();
+    loadEvalFile(evalFileName);
     ucinewgame();
 }
 
@@ -94,6 +96,7 @@ void Uci::uciok() const {
     Output ob{this};
     ob << "id name " << io::app_version << '\n';
     ob << "id author Aleks Peshkov\n";
+    ob << "option name EvalFile type string default " << (evalFileName.empty() ? "<empty>" : evalFileName) << '\n';
     ob << "option name Debug Log File type string default " << (logFileName.empty() ? "<empty>" : logFileName) << '\n';
     ob << "option name Hash type spin"
        << " min "     << ::mebi(tt.minSize())
@@ -107,6 +110,23 @@ void Uci::uciok() const {
 
 void Uci::setoption() {
     consume("name");
+
+    if (consume("EvalFile")) {
+        consume("value");
+
+        inputLine >> std::ws;
+        std::string newEvalFileName;
+        std::getline(inputLine, newEvalFileName);
+
+        if (newEvalFileName == "<empty>") {
+            std::memset(&nnue, 0, sizeof(nnue));
+            evalFileName.clear();
+            return;
+        }
+
+        loadEvalFile(newEvalFileName);
+        return;
+    }
 
     if (consume("Debug Log File")) {
         consume("value");
@@ -142,7 +162,6 @@ void Uci::setoption() {
         return;
     }
 
-
     if (consume("Move Overhead")) {
         consume("value");
 
@@ -172,6 +191,39 @@ void Uci::setoption() {
         io::fail_rewind(inputLine);
         return;
     }
+}
+
+void Uci::loadEvalFile(const std::string& fileName) {
+    std::ifstream file(fileName, std::ios::binary);
+
+    if (!file.is_open()) {
+        log("Error opening EvalFile " + fileName);
+        io::fail_rewind(inputLine);
+        return;
+    }
+
+    file.seekg(0, std::ios::end);
+    if (file.tellg() != sizeof(nnue)) {
+        log("EvalFile size mismatch, expected " + std::to_string(sizeof(nnue)) + ", file size " + std::to_string(file.tellg()));
+        io::fail_rewind(inputLine);
+        return;
+    }
+    file.seekg(std::ios::beg);
+
+    file.read(reinterpret_cast<char*>(&nnue), sizeof(nnue));
+
+    if (!file) {
+        std::memset(&nnue, 0, sizeof(nnue));
+        evalFileName.clear();
+        log("Error reading EvalFile " + fileName);
+        io::fail_rewind(inputLine);
+        file.close();
+        return;
+    }
+    file.close();
+
+    evalFileName = std::move(fileName);
+    return;
 }
 
 void Uci::setHash() {
