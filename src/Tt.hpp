6ee1aa77e35f44b378b90f9fa75167ad5d6bd67a
Aleks Peshkov <aleks.peshkov@gmail.com>
2025-12-13 00:12:59 +0300

    refactoring remove TtPerft.cpp

--- src/Tt.hpp
+++ src/Tt.hpp
@@ -1,46 +1,55 @@
 #ifndef TT_HPP
 #define TT_HPP
 
+#include "bitops.hpp"
+#include "memory.hpp"
 #include "typedefs.hpp"
 #include "Zobrist.hpp"
-
-class HashAge {
-public:
-    using _t = int;
-    enum {AgeBits = 3, AgeMask = (1u << AgeBits)-1};
-
-private:
-    _t v;
-
-public:
-    constexpr HashAge () : v(1) {}
-    constexpr operator const _t& () { return v; }
-
-    void nextAge() {
-        //there are "AgeMask" ages, not "1 << AgeBits", because of:
-        //1) we want to break 4*n ply transposition pattern
-        //2) make sure that initally clear entry is never hidden
-        auto a = (v + 1) & AgeMask;
-        v = a ? a : 1;
-    }
-
-};
 
 class Tt {
     void* memory = nullptr;
     size_t size_ = 0;
 
-    void allocate(size_t);
-    void free();
-    void zeroFill();
+    void free() {
+        if (size_) {
+            ::freeAligned(memory);
+            memory = nullptr;
+            size_ = 0;
+        }
+    }
+
+    void zeroFill() {
+        std::memset(memory, 0, size_);
+    }
+
+    void allocate(size_t _bytes) {
+        const auto minBytes = minSize();
+        auto bytes = ::bit_floor(std::max(_bytes, minBytes));
+
+        if (bytes != size_) {
+            free();
+
+            for (; bytes >= minBytes; bytes >>= 1) {
+                auto ptr = ::allocateAligned(bytes, minBytes);
+
+                if (ptr) {
+                    memory = ptr;
+                    size_ = bytes;
+                    break;
+                }
+            }
+        }
+
+        assert (bytes == size_);
+        zeroFill();
+    }
+
     constexpr uintptr_t mask(size_t align) const { return (size_-1) ^ (align-1); }
 
 public:
     node_count_t reads = 0;
     node_count_t writes = 0;
     node_count_t hits = 0;
-
-    HashAge hashAge;
 
     Tt(size_t n = minSize()) { setSize(n); }
     ~Tt() { free(); }
@@ -49,21 +58,21 @@ public:
 
     // 2MB to trigger linux huge page support if possible
     static constexpr size_t minSize() { return 2 * 1024 * 1024; }
-    static size_t maxSize();
+
+    // all currently available memory
+    static size_t maxSize() { return ::bit_floor(::getAvailableMemory()); }
 
     void setSize(size_t bytes) { allocate(bytes); newGame(); }
-    void newGame() { zeroFill(); hashAge = {}; newSearch(); }
+    void newGame() { zeroFill(); newSearch(); }
     void newSearch() { reads = 0; writes = 0; hits = 0; newIteration(); }
-    void newIteration() { hashAge.nextAge(); }
-
-    const HashAge& getAge() const { return hashAge; }
-    void nextAge() { hashAge.nextAge(); }
+    void newIteration() {}
 
     constexpr void* addr(ZArg z, size_t align) const {
         return static_cast<void*>(static_cast<char*>(memory) + (z & mask(align)));
     }
 
-    template <typename T> constexpr T* addr(ZArg z) const {
+    template <typename T>
+    constexpr T* addr(ZArg z) const {
         return static_cast<T*>( addr(z, sizeof(T)) );
     }
 
@@ -73,7 +82,8 @@ public:
         return ptr;
     }
 
-    template <typename T> T* prefetch(ZArg z) const {
+    template <typename T>
+    T* prefetch(ZArg z) const {
         return static_cast<T*>( prefetch(z, sizeof(T)) );
     }
 
