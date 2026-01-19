1cfb999c44f53a5499367ca62f41d15331fc1f6c
Aleks Peshkov <aleks.peshkov@gmail.com>
2026-01-19 03:54:53 +0300

    TimeInterval UciSearchLimits::deadline[]; remove UciSearchLimits.hpp

--- src/Uci.hpp
+++ src/Uci.hpp
@@ -1,15 +1,218 @@
 #ifndef UCI_HPP
 #define UCI_HPP
 
+#include <atomic>
 #include <fstream>
 #include <mutex>
+#include "chrono.hpp"
 #include "io.hpp"
+#include "Index.hpp"
 #include "Repetitions.hpp"
 #include "Thread.hpp"
 #include "Tt.hpp"
 #include "UciPosition.hpp"
 #include "UciMove.hpp"
-#include "UciSearchLimits.hpp"
+
+enum deadline_t { HardDeadline, RootMoveDeadline, IterationDeadline };
+
+class UciSearchLimits {
+    friend class Uci; // for bench()
+
+    constexpr static int QuotaLimit = 1000;
+
+    mutable node_count_t nodes = 0; // (0 <= nodes && nodes <= nodesLimit)
+    mutable node_count_t nodesLimit = NodeCountMax; // search limit
+
+    //number of remaining nodes before slow checking for search stop
+    mutable int nodesQuota = 0; // (0 <= nodesQuota && nodesQuota <= QuotaLimit)
+
+    mutable std::atomic_bool stop_{false};
+    mutable std::atomic_bool infinite{false};
+    mutable std::atomic_bool ponder{false};
+
+    TimePoint searchStartTime;
+
+    TimeInterval deadline[3] = {
+        TimeInterval::max(), // HardDeadline      : tested every QuotaLimit nodes
+        TimeInterval::max(), // RootMoveDeadline  : tested after every root move search ends
+        TimeInterval::max()  // IterationDeadline : tested when iteration ends
+    };
+
+    Side::arrayOf<TimeInterval> time = {{ 0ms, 0ms }};
+    Side::arrayOf<TimeInterval> inc = {{ 0ms, 0ms }};
+    TimeInterval movetime = 0ms;
+
+    int movestogo = 0;
+    int mate = 0;
+
+    constexpr void assertNodesOk() const {
+        assert (0 <= nodesQuota);
+        assert (nodesQuota < QuotaLimit);
+        //assert (0 <= nodes);
+        assert (nodes <= nodesLimit);
+        assert (static_cast<decltype(nodesLimit)>(nodesQuota) <= nodes);
+    }
+
+    constexpr void setNoDeadline() {
+        deadline[HardDeadline]      = TimeInterval::max();
+        deadline[RootMoveDeadline]  = TimeInterval::max();
+        deadline[IterationDeadline] = TimeInterval::max();
+    }
+
+    constexpr TimeInterval average(Side si) const {
+        assert (movestogo >= 0);
+
+        if (!movestogo && inc[si] == 0ms) {
+            return time[si] / 25; // sudden death
+        }
+
+        auto moves = movestogo ? std::min(movestogo, 20) : 20;
+        return inc[si] + (time[si]-inc[si])/moves;
+    }
+
+    void setSearchDeadline() {
+        searchStartTime = timeNow();
+
+        if (infinite.load(std::memory_order_relaxed)) {
+            setNoDeadline();
+            return;
+        }
+        if (movetime > 0ms) {
+            setNoDeadline();
+            deadline[HardDeadline] = movetime - moveOverhead;
+            return;
+        }
+        if (time[Side{My}] == 0ms) {
+            setNoDeadline();
+            return;
+        }
+
+        // average remaining time per move
+        auto myAverage = average(Side{My}) + (canPonder ? average(Side{Op}) / 2 : 0ms);
+        auto hardInterval = std::min(time[Side{My}], myAverage * 2) - moveOverhead;
+        hardInterval = std::max(TimeInterval{0}, hardInterval);
+
+        deadline[HardDeadline]      = hardInterval;   // 200% average time
+        deadline[RootMoveDeadline]  = hardInterval/2; // 100% average time
+        deadline[IterationDeadline] = hardInterval/4; // 50% average time
+    }
+
+    ReturnStatus refreshQuota() const {
+        assertNodesOk();
+        nodes -= nodesQuota;
+
+        auto nodesRemaining = nodesLimit - nodes;
+        if (nodesRemaining >= QuotaLimit) {
+            nodesQuota = QuotaLimit;
+        }
+        else {
+            nodesQuota = static_cast<decltype(nodesQuota)>(nodesRemaining);
+            if (nodesQuota == 0) {
+                assertNodesOk();
+                return ReturnStatus::Stop;
+            }
+        }
+
+        if (reached<HardDeadline>()) {
+            nodesLimit = nodes;
+            nodesQuota = 0;
+
+            assertNodesOk();
+            return ReturnStatus::Stop;
+        }
+
+        assert (0 < nodesQuota && nodesQuota <= QuotaLimit);
+        nodes += nodesQuota;
+        --nodesQuota; //count current node
+
+        assertNodesOk();
+        return ReturnStatus::Continue;
+    }
+
+public:
+    Ply depth{MaxPly};
+
+    bool canPonder{false};
+
+    TimeInterval moveOverhead = 100us;
+
+    // clear all limits except canPonder and moveOverhead
+    void clear() {
+        nodes = 0;
+        nodesLimit = NodeCountMax;
+        nodesQuota = 0;
+        setNoDeadline();
+        movetime = 0ms;
+        time = {{ 0ms, 0ms }};
+        inc = {{ 0ms, 0ms }};
+        depth = MaxPly;
+        movestogo = 0;
+        mate = 0;
+        ponder.store(false, std::memory_order_relaxed);
+        infinite.store(false, std::memory_order_relaxed);
+        stop_.store(false, std::memory_order_relaxed);
+    }
+
+    istream& go(istream&, Side);
+
+    void ponderhit() {
+        ponder.store(false, std::memory_order_relaxed);
+        reached<HardDeadline>();
+    }
+
+    void stop() const {
+        infinite.store(false, std::memory_order_relaxed);
+        ponder.store(false, std::memory_order_relaxed);
+        stop_.store(true, std::memory_order_release);
+    }
+
+    bool isStopped() const {
+        return stop_.load(std::memory_order_acquire);
+    }
+
+    // ponder || infinite
+    bool shouldDelayBestmove() {
+        return ponder.load(std::memory_order_relaxed) || infinite.load(std::memory_order_relaxed);
+    }
+
+    template <deadline_t DeadlineKind>
+    bool reached() const {
+        if (isStopped()) { return true; }
+
+        bool isDeadlineReached =
+            !ponder.load(std::memory_order_relaxed)
+            && deadline[DeadlineKind] != TimeInterval::max()
+            && deadline[DeadlineKind] < elapsedSinceStart()
+        ;
+
+        if (isDeadlineReached) { stop(); }
+        return isDeadlineReached;
+    }
+
+    TimeInterval elapsedSinceStart() const {
+        return ::elapsedSince(searchStartTime);
+    }
+
+    /// exact number of visited nodes
+    constexpr node_count_t getNodes() const {
+        assertNodesOk();
+        return nodes - nodesQuota;
+    }
+
+    ReturnStatus countNode() const {
+        assertNodesOk();
+
+        if (nodesQuota == 0 || isStopped()) {
+            return refreshQuota();
+        }
+
+        assert (nodesQuota > 0);
+        --nodesQuota;
+
+        assertNodesOk();
+        return ReturnStatus::Continue;
+    }
+};
 
 class HistoryMoves {
     using Index = ::Index<2>;
