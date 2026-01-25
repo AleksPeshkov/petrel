797ab7353be6feb5ca9f61f9034869d2bf0c152b
Aleks Peshkov <aleks.peshkov@gmail.com>
2026-01-25 20:53:02 +0300

    NNUE: incremental Position::Accumulator update

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include "io.hpp"
+#include "nnue.hpp"
 #include "Bb.hpp"
 #include "Hyperbola.hpp"
 #include "PiMask.hpp"
 #include "Score.hpp"
 #include "Uci.hpp"
+
+// global almost constant instance
+Nnue nnue;
 
 /**
 * Startup constant initialization
