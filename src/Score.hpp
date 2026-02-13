67321806db3ed7d84eb638c172d7e4311615bae6
Aleks Peshkov <aleks.peshkov@gmail.com>
2026-02-13 18:50:38 +0300

    iteration time low material bonus

--- src/Score.hpp
+++ src/Score.hpp
@@ -366,6 +366,12 @@ public:
     constexpr bool canNullMove() const {
         return v_.s.officers > 0;
     }
+
+    // [0..6] startpos = 6, queens exchanged = 4, R vs R endgame = 1
+    static constexpr int gamePhase(Evaluation my, Evaluation op) {
+        auto phase = (my.v_.s.officers + op.v_.s.officers) / 12;
+        return std::min(phase, 6);
+    }
 };
 
 #endif
