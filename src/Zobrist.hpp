5203c3edfa484e267170833f84bad25a9a6cad0e
Aleks Peshkov <aleks.peshkov@gmail.com>
2026-02-25 12:40:53 +0300

    refactoring PieceType

--- src/Zobrist.hpp
+++ src/Zobrist.hpp
@@ -44,8 +44,15 @@ class Zobrist : public Z {
         // ZExtra  = U64(0x03ac'4dfb'4854'6797), // reserved
     };
 
-    enum { Castling = 6, EnPassant = 7 };
-    using Index = ::Index<8>;
+    enum zobrist_index_t { Castling = 6, EnPassant = 7 };
+    struct Index : ::Index<8> {
+        using Base = ::Index<8>;
+        constexpr Index (PieceType::_t ty) : Base{ty} {}
+        constexpr Index (zobrist_index_t ty) : Base{ty} {}
+        constexpr Index (PieceType ty) : Base{ty} {}
+        constexpr Index (NonKingType ty) : Base{ty} {}
+        constexpr Index (PromoType ty) : Base{ty} {}
+    };
 
     constexpr static Index::arrayOf<_t> zKey{
         ZQueen, ZRook, ZBishop, ZKnight, ZPawn, ZKing, ZCastling, ZEnPassant
@@ -61,30 +68,30 @@ class Zobrist : public Z {
 public:
     using Z::Z;
     constexpr Zobrist (Zobrist my, Zobrist op) : Z{my.v ^ flip(op)} {
-        static_assert (z(Index{EnPassant}, Square{A4}) == z(Index{Pawn}, Square{A8}));
-        static_assert (z(Index{EnPassant}, Square{B4}) == z(Index{Pawn}, Square{B8}));
+        static_assert (z(EnPassant, Square{A4}) == z(Pawn, Square{A8}));
+        static_assert (z(EnPassant, Square{B4}) == z(Pawn, Square{B8}));
     }
 
     constexpr Zobrist& flip() { v = ::byteswap(v); return *this; }
 
-    void operator () (PieceType ty, Square sq) { my(Index{ty}, sq); }
-    void castling(Square sq)  { assert (sq.on(Rank1)); my(Index{Castling}, sq); }
-    void enPassant(Square sq) { assert (sq.on(Rank4)); my(Index{EnPassant}, sq); }
+    void operator () (PieceType ty, Square sq) { my(ty, sq); }
+    void castling(Square sq)  { assert (sq.on(Rank1)); my(Castling, sq); }
+    void enPassant(Square sq) { assert (sq.on(Rank4)); my(EnPassant, sq); }
 
-    void op(NonKingType ty, Square sq) { op(Index{ty}, sq);}
-    void opCastling(Square sq)  { assert (sq.on(Rank1)); op(Index{Castling}, sq); }
-    void opEnPassant(Square sq) { assert (sq.on(Rank4)); op(Index{EnPassant}, sq); }
+    void opCapture(NonKingType ty, Square sq) { op(ty, sq); }
+    void opCastling(Square sq)  { assert (sq.on(Rank1)); op(Castling, sq); }
+    void opEnPassant(Square sq) { assert (sq.on(Rank4)); op(EnPassant, sq); }
 
     void move(PieceType ty, Square from, Square to) {
         assert (from != to);
-        my(Index{ty}, from);
-        my(Index{ty}, to);
+        my(ty, from);
+        my(ty, to);
     }
 
     void promote(Square from, PromoType ty, Square to) {
         assert (from.on(Rank7) && to.on(Rank8));
-        my(Index{Pawn}, from);
-        my(Index{ty}, to);
+        my(Pawn, from);
+        my(ty, to);
     }
 
     void castle(Square kingFrom, Square kingTo, Square rookFrom, Square rookTo) {
@@ -92,10 +99,10 @@ public:
         assert (rookFrom.on(Rank1) && rookTo.on(Rank1));
         assert (kingFrom != rookFrom);
         assert (kingTo != rookTo);
-        my(Index{King}, kingFrom);
-        my(Index{King}, kingTo);
-        my(Index{Rook}, rookFrom);
-        my(Index{Rook}, rookTo);
+        my(King, kingFrom);
+        my(King, kingTo);
+        my(Rook, rookFrom);
+        my(Rook, rookTo);
     }
 
 };
