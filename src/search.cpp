39ac41254725294aa07c9c6f16b25f4f4d3d3ad3
Aleks Peshkov <aleks.peshkov@gmail.com>
2026-04-20 22:23:10 +0300

    LMP d=1, m>=3

--- src/search.cpp
+++ src/search.cpp
@@ -342,6 +342,8 @@ ReturnStatus Node::search() {
         // safe pawns pushes attacking non-pawns
         RETURN_CUTOFF (goodPawnsMovesTo(~(OP.bbSide() - OP.bbPawns()), 3_ply));
 
+        if (depth <= 1_ply && !inCheck() && movesMade() >= 3) { break; }
+
         // safe officers moves
         while (safePieces.any()) {
             Pi pi = safePieces.piLast(); safePieces -= pi;
