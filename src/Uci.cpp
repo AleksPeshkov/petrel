6bca7d8cf25058bc22338b744a569f5833951310
Aleks Peshkov <aleks.peshkov@gmail.com>
2026-05-08 18:53:57 +0300

    refactoring UciSearchLimits; IterationQuota = 12; Move Overhead 1.5

--- src/Uci.cpp
+++ src/Uci.cpp
@@ -200,6 +200,13 @@ UciOutput& operator << (UciOutput& ob, const PrincipalVariation& pv) {
     return ob;
 }
 
+UciOutput& info_pv(UciOutput& ob, const PrincipalVariation& pv, const UciSearchLimits& limits) {
+    ob << "info depth " << pv.depth();
+    limits.nps(ob);
+    ob << pv;
+    return ob;
+}
+
 istream& operator >> (istream& is, Square& sq) {
     auto before = is.tellg();
 
@@ -993,8 +1000,7 @@ void Uci::go() {
     debugGo = inputLine.str();
 #endif
 
-    limits.go(inputLine, position_.sideOf(White), &position_);
-    if (consume("searchmoves")) { position_.limitMoves(inputLine); }
+    limits.go(inputLine, position_);
 
     auto started = mainSearchThread.start([this] {
         Node{position_, *this}.searchRoot();
@@ -1045,14 +1051,7 @@ void Uci::wait() {
 
 void Uci::info_pv() const {
     UciOutput ob{this};
-    info_pv(ob);
-}
-
-UciOutput& Uci::info_pv(UciOutput& ob) const {
-    ob << "info depth " << pv.depth();
-    limits.nps(ob);
-    ob << pv;
-    return ob;
+    ::info_pv(ob, pv, limits);
 }
 
 void Uci::info_bestmove() const {
@@ -1060,7 +1059,7 @@ void Uci::info_bestmove() const {
     auto delayed = limits.shouldDelayBestmove();
 
     if (limits.hasNewNodes()) {
-        info_pv(ob);
+        ::info_pv(ob, pv, limits);
         if (delayed) { ob.flush(); } else { ob << '\n'; }
     }
 
@@ -1087,9 +1086,9 @@ ostream& Uci::info_fen(ostream& os) const {
 }
 
 void Uci::info_readyok() const {
-    Output ob{this};
-    limits.info_nps(ob);
+    UciOutput ob{this};
     ob << "readyok";
+    if (limits.hasNewNodes()) { ob << '\n'; ::info_pv(ob, pv, limits); }
 }
 
 void Uci::goPerft() {
@@ -1107,7 +1106,7 @@ void Uci::goPerft() {
 
 void Uci::info_perft_bestmove() const {
     Output ob{this};
-    limits.info_nps(ob);
+    if (limits.hasNewNodes()) { ob << "info"; limits.nps(ob) << '\n'; }
     ob << "bestmove 0000";
 }
 
@@ -1188,7 +1187,7 @@ void Uci::bench(std::string& goLimits) {
         newGame();
         newSearch();
 
-        limits.go(is, position_.sideOf(White));
+        limits.go(is, position_);
 
         Node{position_, *this}.searchRoot();
         benchNodes += limits.getNodes();
